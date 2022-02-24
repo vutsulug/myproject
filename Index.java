67589a2dc072a7edcdc175ad1f707377f80b1bc6
vutsulug <nyikovutsulu@gmail.com>
2022-02-24 13:08:48 +0200

    Commit sort numbers

--- Index.java
+++ Index.java
@@ -1,7 +1,26 @@
 
 public class Index {
   public static void main(String[] args) {
+    
+    int[] myNum = {20, 10, 40, 90, 50};
+    int tempSort;
 
-    System.out.print("Hello World");
+    for (int i = 0; i < myNum.length; i++) {
+            
+        for (int j = 0; j < myNum.length; j++) {
+            if(myNum[i] < myNum[j]){
+                
+                tempSort = myNum[i];
+                myNum[i] = myNum[j]; 
+                myNum[j] = tempSort;
+            }
+        }
+    }
+
+    //Output
+    for (int x = 0; x < myNum.length; x++){
+        System.out.println(myNum[x]);
+    }
+
   }
 }
